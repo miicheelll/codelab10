4f1e9e893e1f11c7991e0b361be093714cd3d759
Michelle Dorani Shiba <doranimichelle@gmail.com>
2025-11-05 10:02:18 +0700

    prak

---
+++ master_plan/lib/views/plan_creator_screen.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import '../provider/plan_provider.dart';
+import '../models/data_layer.dart';
+import './plan_screen.dart';
+
+class PlanCreatorScreen extends StatefulWidget {
+  const PlanCreatorScreen({super.key});
+
+  @override
+  State createState() => _PlanCreatorScreenState();
+}
+
+class _PlanCreatorScreenState extends State<PlanCreatorScreen> {
+  final textController = TextEditingController();
+
+  @override
+  void dispose() {
+    textController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // Ganti ‘Namaku’ dengan nama panggilan kamu
+      appBar: AppBar(title: const Text('Master Plans Michelle')),
+      body: Column(
+        children: [
+          _buildListCreator(),
+          Expanded(child: _buildMasterPlans()),
+        ],
+      ),
+    );
+  }
+
+  // Input untuk menambah Plan baru
+  Widget _buildListCreator() {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Material(
+        color: Theme.of(context).cardColor,
+        elevation: 10,
+        child: TextField(
+          controller: textController,
+          decoration: const InputDecoration(
+            labelText: 'Add a plan',
+            contentPadding: EdgeInsets.all(20),
+          ),
+          onEditingComplete: addPlan,
+        ),
+      ),
+    );
+  }
+
+  // Menambahkan plan baru ke provider
+  void addPlan() {
+    final text = textController.text;
+    if (text.isEmpty) return;
+
+    final plan = Plan(name: text, tasks: []);
+    ValueNotifier<List<Plan>> planNotifier = PlanProvider.of(context);
+
+    planNotifier.value = List<Plan>.from(planNotifier.value)..add(plan);
+    textController.clear();
+    FocusScope.of(context).requestFocus(FocusNode());
+    setState(() {});
+  }
+
+  // Menampilkan semua plan
+  Widget _buildMasterPlans() {
+    ValueNotifier<List<Plan>> planNotifier = PlanProvider.of(context);
+    List<Plan> plans = planNotifier.value;
+
+    if (plans.isEmpty) {
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          const Icon(Icons.note, size: 100, color: Colors.grey),
+          Text(
+            'Anda belum memiliki rencana apapun.',
+            style: Theme.of(context).textTheme.headlineSmall,
+          ),
+        ],
+      );
+    }
+
+    return ListView.builder(
+      itemCount: plans.length,
+      itemBuilder: (context, index) {
+        final plan = plans[index];
+        return ListTile(
+          title: Text(plan.name),
+          subtitle: Text(plan.completenessMessage),
+          onTap: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (_) => PlanScreen(plan: plan),
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+}
